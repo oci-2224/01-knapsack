6c0366b5880021fcb0bc59ea26ad0d53a6a78b1e
Cédric Donner <cedonner@gmail.com>
2024-05-01 07:29:47 +0000

    better start

--- src/brute_force.py
+++ src/brute_force.py
@@ -3,6 +3,21 @@
 from knapsack import KnapsackInstance, KnapsackSolver
 
 class BruteforceKnapsackSolver(KnapsackSolver):
+    '''
+    >>> kp = KnapsackInstance(W=[13, 13, 13, 10, 24, 11], V=[2600, 2600, 2600, 500, 4500, 960], C=50)
+    >>> bfs = BruteforceKnapsackSolver(kp)
+    >>> Xopt = bfs.solve()
+    >>> Xopt
     
-    def solve(self):
+    '''
+    
+    def __init__(self, instance) -> None:
+        super().__init__(instance)
+        
+    def all_possibilities(self):
         ...
+    
+    def solve(self) -> list[int]:
+        ...
+        
+        return []
